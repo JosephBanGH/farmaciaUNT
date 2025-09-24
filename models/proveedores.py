7b009a75a1890b673c24bf7ca69f3fbb8c3f2579
joseplocal <joseplocal>
2025-09-23 21:06:23 -0500

    SCRUM-16 todos los modelos creados

---
+++ models/proveedores.py
@@ -0,0 +1,9 @@
+class Proveedores:
+    def __init__(self, id, nombre, contacto, telefono, email, direccion, fecha_creacion):
+        self.id = id
+        self.nombre = nombre
+        self.contacto = contacto
+        self.telefono = telefono
+        self.email = email
+        self.direccion = direccion
+        self.fecha_creacion = fecha_creacion
---
+++ models/ventas.py
@@ -0,0 +1,8 @@
+class Ventas:
+    def __init__(self, id, cliente_id, total, impuesto, usuario_id, fecha_venta):
+        self.id = id
+        self.cliente_id = cliente_id  
+        self.total = total
+        self.impuesto = impuesto
+        self.usuario_id = usuario_id  
+        self.fecha_venta = fecha_venta
