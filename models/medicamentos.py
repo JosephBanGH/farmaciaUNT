f85ed678cfdf3c8066012d2e72c5481d12708deb
joseplocal <joseplocal>
2025-09-23 20:35:51 -0500

    SCRUM-16 entidad medicamentos

---
+++ models/medicamentos.py
@@ -0,0 +1,14 @@
+class Medicamentos:
+    def __init__(self,id,descripcion,nombre,principio_activo,laboratorio,precio,stock,stock_minimo,lote,fecha_vencimiento,fecha_creacion,activo):
+        self.id = id
+        self.descripcion = descripcion
+        self.nombre = nombre
+        self.principio_activo = principio_activo
+        self.laboratorio = laboratorio
+        self.precio = precio
+        self.stock = stock
+        self.stock_minimo = stock_minimo
+        self.lote = lote
+        self.fecha_vencimiento = fecha_vencimiento
+        self.fecha_creacion = fecha_creacion
+        self.activo = activo
