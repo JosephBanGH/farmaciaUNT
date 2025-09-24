22c6b5c904f4390e771c2b2e1cae9920bbf9273c
joseplocal <joseplocal>
2025-09-23 21:07:24 -0500

    SCRUM-16 comentarios boorrados

--- models/movimiento_inventario.py
+++ models/movimiento_inventario.py
@@ -1,14 +1,3 @@
-# CREATE TABLE movimientos_inventario (
-#     id INT AUTO_INCREMENT PRIMARY KEY,
-#     medicamento_id INT NOT NULL,
-#     tipo ENUM('entrada', 'salida') NOT NULL,
-#     cantidad INT NOT NULL,
-#     motivo VARCHAR(200),
-#     usuario_id INT NOT NULL,
-#     fecha_movimiento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-#     FOREIGN KEY (medicamento_id) REFERENCES medicamentos(id),
-#     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-# );
 class Movimiento_inventario():
     def __init__(self,id,medicamento_id,tipo,cantidad,motivo,usuario_id,fecha_movimiento):
         self.id = id
